a5e82c69f722c162c393947799753697407f7fd2
dk241294 <49711366+dk241294@users.noreply.github.com>
2019-07-01 19:11:17 +0530

    currency conversion from dollar to rupee

---
+++ currencyconverter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void conversion(View view){
+        EditText editText=findViewById(R.id.editText);
+        EditText editText2=findViewById(R.id.editText2);
+        String dollar = editText.getText().toString();
+        Double ruppee = Double.parseDouble(dollar)*72;
+        String inrupee = Double.toString(ruppee);
+        editText.setText(dollar);
+        editText2.setText(inrupee);
+        Toast.makeText(this, "$ "+dollar+" in rupee "+ ruppee,  Toast.LENGTH_SHORT).show();;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
