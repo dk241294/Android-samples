f2fda6501715fe0da90322209b0c64b9e77f1f40
dk241294 <49711366+dk241294@users.noreply.github.com>
2019-07-04 23:00:02 +0530

    coffee house
    drinks ,food ,stores

---
+++ CoffeeHouse/app/src/main/java/com/example/coffeehouse/Drink.java
@@ -0,0 +1,31 @@
+package com.example.coffeehouse;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static Drink[] drinks ={
+            new Drink("Latte","a couple of espresso shots with steamed milk",R.drawable.latte),
+            new Drink("cappuccino","espresso,hot milk, and a steamed milk foam",R.drawable.cappuccino),
+            new Drink("Filter","Highest quality beans roasted and brewed fresh",R.drawable.filter)
+    };
+    private Drink(String name,String description,int imageResourceId){
+        this.name =name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public  String getDescription(){
+        return  description;
+    }
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+    public String toString(){
+        return this.name;
+    }
+}
